fdff21761619515fcd0507a7769adf1baa9477ca
Boris Poloshkov <bpoloshkov@ozon.ru>
2024-12-01 21:24:38 +0300

    add readme, make convolution-like algorithm impl

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,7 @@
 import argparse
+import time
+from typing import Callable, TypeVar
+
 import cv2
 
 from lab1.impl import cv_adaptive_thresholding_mean, our_adaptive_thresholding_mean
@@ -12,25 +15,33 @@ def main():
 
     image = cv2.imread(args["image"])
 
-    # # Оригинал
-    # cv2.imshow("Image", image)
-    # cv2.waitKey()
+    # Оригинал
+    cv2.imshow("Image", image)
+    cv2.waitKey()
 
     # Конвертируем в черно-белый
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("Grayscale", gray)
-    # cv2.waitKey()
+    cv2.imshow("Grayscale", gray)
+    cv2.waitKey()
 
     # Реализация opencv
-    cv_thresh = cv_adaptive_thresholding_mean(gray)
+    time_cv_thresh, cv_thresh = time_elapsed(cv_adaptive_thresholding_mean, gray)
+    print(f'Time elapsed on opencv implementation: {time_cv_thresh}')
     cv2.imshow("CV Adaptive Thresholding", cv_thresh)
     cv2.waitKey()
 
     # Собственная реализация
-    our_thresh = our_adaptive_thresholding_mean(gray)
+    time_our_thresh, our_thresh = time_elapsed(our_adaptive_thresholding_mean, gray)
+    print(f'Time elapsed on our implementation: {time_our_thresh}')
     cv2.imshow("Our Adaptive Thresholding", our_thresh)
     cv2.waitKey()
 
+T = TypeVar('T')
+def time_elapsed(f: Callable[[...], T], *args, **kwargs) -> tuple[float, T]:
+    start = time.time()
+    res = f(*args, **kwargs)
+    end = time.time()
+    return end - start, res
 
 if __name__ == '__main__':
     main()
